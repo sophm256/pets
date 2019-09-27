71b8834e69d1f4caab1645ea9e77c335a99f05a6
Sophan Mien <53249319+sophm256@users.noreply.github.com>
2019-09-27 11:58:12 -0500

    First commit: created 'core' app inside 'mysite', created signup feature

---
+++ mysite/core/admin.py
@@ -0,0 +1,4 @@
+from django.contrib import admin
+from mysite.core.models import CustomUser
+# Register your models here.
+admin.site.register(CustomUser)
---
+++ mysite/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render, get_object_or_404
+
+
+# Create your views here.
+def index(request):
+   
+    return render(request, 'mysite/index.html', {})
+
