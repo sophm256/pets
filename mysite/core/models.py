05157176fc510fbf1f5f52a90c37188ecb6b1c02
Sophan Mien <53249319+sophm256@users.noreply.github.com>
2019-10-25 21:41:24 -0500

    Changed models.py so that 'email' field is unique in CustomUser model

--- mysite/core/models.py
+++ mysite/core/models.py
@@ -5,3 +5,6 @@ class CustomUser(AbstractUser):
     phone = models.CharField(max_length=10, blank=True)
     
     REQUIRED_FIELDS = ['email']
+
+    class Meta(object):
+    	unique_together=('email',)
