26d295514eea33c8d6f103688f52c25574d7cf4b
Sophan Mien <53249319+sophm256@users.noreply.github.com>
2019-11-22 16:22:28 -0600

    Added mapbox capability to mymap. Also changed base template to account for javascript and css code blocks separately

--- maps/views.py
+++ maps/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from django.utils.safestring import mark_safe
 import json
+from mysite.settings import MY_TEST_VAR, MAPBOX_ACCESS_TOKEN
 
 # Create your views here.
 def mymap_orig(request):
@@ -11,5 +12,10 @@ def mymap_orig(request):
 #IMPORTANT:Should we be marking it safe??
 def mymap(request, room_name):
     return render(request, 'maps/mymap.html', {
-        'room_name_json': mark_safe(json.dumps(room_name))
+        'room_name_json': mark_safe(json.dumps(room_name)),
+        'my_test_var': MY_TEST_VAR,
+        'mapbox_access_token': MAPBOX_ACCESS_TOKEN
+       
+
     })
+    
