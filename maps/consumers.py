591b62659a822735d759d91eb71f5f62613e9022
Sophan Mien <53249319+sophm256@users.noreply.github.com>
2019-11-29 19:45:20 -0600

    Changed datetime in maps page and consumer page. Added ability to record start and stop time in database

--- maps/consumers.py
+++ maps/consumers.py
@@ -1,20 +1,21 @@
 from asgiref.sync import async_to_sync
 from channels.generic.websocket import WebsocketConsumer
 import json
-
-
+from mysite.core.models import TrackingCoord, StartStopTime, CustomUser, SearchPartyInstance, SearchPartyMembers
+from django.contrib.gis.geos import fromstr
+import datetime
 
 class MapsConsumer(WebsocketConsumer):
-    coord_dict ={'alan':[[-87, 43]]}
+    #coord_dict ={'alan':[[-87, 43]]}
     # def __init__(self):
     #     super().__init__()
     #     self.coord_dict ={'alan':[[-87, 43]]}
 
-    def addToDict(self, username, coord):
-        if username in self.coord_dict:
-            self.coord_dict[username].append(coord)
-        else:
-            self.coord_dict[username]= [coord]
+    # def addToDict(self, username, coord):
+    #     if username in self.coord_dict:
+    #         self.coord_dict[username].append(coord)
+    #     else:
+    #         self.coord_dict[username]= [coord]
     
     def connect(self):
         self.room_name = self.scope['url_route']['kwargs']['room_name']
@@ -38,10 +39,39 @@ class MapsConsumer(WebsocketConsumer):
     # Receive message from WebSocket
     def receive(self, text_data):
         text_data_json = json.loads(text_data)
-        coord = text_data_json['coord']
-        username = text_data_json['username']
+        
+        message_type = text_data_json["message_type"]
 
-        self.addToDict(username,coord)
+        username = text_data_json['username']
+        search_party_instance_id = text_data_json['search_party_instance_id']
+        custom_user = CustomUser.objects.get(username=username)
+        membership= SearchPartyMembers.objects.get(member=custom_user)
+        js_timestamp = text_data_json['time_stamp']
+        date_time = datetime.datetime.fromtimestamp(js_timestamp)
+
+        if (message_type == 'coordinate'):
+            coord = text_data_json['coord']
+            longitude = coord[0]
+            latitude = coord[1]
+            
+            # Add the coordinate to the TrackingCoord table
+            my_point = fromstr(f'POINT({longitude} {latitude})', srid=4326)
+            tracking_coord = TrackingCoord.objects.create(search_party_member=membership, my_point=my_point, date_time=date_time)
+            tracking_coord.save()
+        
+        elif (message_type == 'start_tracking' or message_type == 'stop_tracking'):
+            if (message_type == 'start_tracking'):
+                start_or_stop_type = 1
+            elif (message_type == 'stop_tracking'):
+                start_or_stop_type = 2
+            start_stop_time = StartStopTime.objects.create(search_party_member=membership, datetime_start_or_stop=date_time, start_or_stop_type=start_or_stop_type)
+            start_stop_time.save()
+
+        # Get all of the coordinates for that particular member from the database
+        #TODO
+        #TrackingCoord.objects.
+
+        # self.addToDict(username,coord)
 
         # Send message to room group
         async_to_sync(self.channel_layer.group_send)(
@@ -49,19 +79,21 @@ class MapsConsumer(WebsocketConsumer):
             {
                 'type': 'chat_message',
                 'username': username,
-                'coordinates': self.coord_dict[username]
+                #TODO 'coordinates': self.coord_dict[username]
+                'coordinates': [4,5]
             }
         )
 
     # Receive message from room group
     def chat_message(self, event):
         username = event['username']
+       #TODO coordinates = event['coordinates']
         coordinates = event['coordinates']
-
         # Send message to WebSocket
         self.send(text_data=json.dumps({
             'username': username,
-            'coordinates': coordinates
+        #TODO  'coordinates': coordinates
+        'coordinates': coordinates     
         }))
 
     
--- maps/views.py
+++ maps/views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, get_object_or_404
 from django.utils.safestring import mark_safe
 import json
@@ -10,9 +11,11 @@ def mymap_orig(request):
 
 
 #IMPORTANT:Should we be marking it safe??
+@login_required
 def mymap(request, room_name):
     return render(request, 'maps/mymap.html', {
         'room_name_json': mark_safe(json.dumps(room_name)),
+        'room_name': str(room_name),
         'my_test_var': MY_TEST_VAR,
         'mapbox_access_token': MAPBOX_ACCESS_TOKEN
        
